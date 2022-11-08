7297f85c207faaad1de8afb4edb012a264fa2d60
HP <HP@LAPTOP-EK6SKHTS>
2022-11-08 22:26:45 +0800

    hai

---
+++ src/project/awal/ProjectAwal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.awal;
+
+/**
+ *
+ * @author HP
+ */
+public class ProjectAwal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Halo gais!");
+       System.out.println ("Nama gue Natasha femi");
+       System.out.println ("oke cukup sekian");
+    }   
+    
+    
+}
